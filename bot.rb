5519f6afc61fe40af9b58b50833abd5de71ff677
brianbarellaa <53068897+brianbarellaa@users.noreply.github.com>
2020-03-04 14:46:24 +0700

    Add files via upload

--- bot.rb
+++ bot.rb
@@ -14,6 +14,8 @@ bot.get_updates(fail_silently: true) do |message|
       reply.text = "All I can do is say hello. Try the /greet command."
     when /greet/i
       reply.text = "Hello, #{message.from.first_name}. 🤖"
+    when /jumlah/i
+      reply.text = "Jumlah Material :, #{message.from.username}"
     else
       reply.text = "I have no idea what #{command.inspect} means."
     end
