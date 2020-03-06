22b02f9c665fa3d28a73526a924c8ec08779dda0
brianbarellaa <53068897+brianbarellaa@users.noreply.github.com>
2020-03-06 16:13:30 +0700

    Add files via upload

--- bot.rb
+++ bot.rb
@@ -1,8 +1,18 @@
 require 'telegram_bot'
+require "mysql2/em" 
 
 token = '1136297398:AAEKKPU_f01jIa3ZIlvE7c1bka2a67uIPOA'
 
 bot = TelegramBot.new(token: token)
+
+CREATE TABLE `Assurance`.`Monitor` (`No.` INT NOT NULL AUTO_INCREMENT, `Tiket` TEXT(45), `Layanan` TEXT(45), `Progress` TEXT(45), `Pelanggan` TEXT(45), `Teknisi` TEXT(45));
+
+@db_host  = "localhost"
+@db_user  = "root"
+@db_pass  = "whatever"
+@db_name = "Assurance"
+
+client = Mysql2::Client.new(:host => @db_host , :username => @db_user, :password => @db_pass, :database =>@db_name)
 
 bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
@@ -10,12 +20,16 @@ bot.get_updates(fail_silently: true) do |message|
 
   message.reply do |reply|
     case command
-    when /start/i
-      reply.text = "All I can do is say hello. Try the /greet command."
-    when /greet/i
+    when /command1/i
+      # Retrieve all rows from the persons table
+      results = client.query("SELECT * FROM Monitor")
+    when /command2/i
       reply.text = "Hello, #{message.from.first_name}. 🤖"
-    when /jumlah/i
+    when /command3/i
       reply.text = "Jumlah Material :, #{message.from.username}"
+    when /command4/i
+      # We insert some data using MySQL multi-row insert
+      client.query("insert into Monitor(Tiket) values('Mark'), ('Osman'), ('Conan')")
     else
       reply.text = "I have no idea what #{command.inspect} means."
     end
