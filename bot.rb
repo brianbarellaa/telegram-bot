d1b8a9257e97359d68f1b9d3713ddd0faa24204e
brianbarellaa <53068897+brianbarellaa@users.noreply.github.com>
2020-03-06 10:02:46 +0700

    Add files via upload

--- bot.rb
+++ bot.rb
@@ -8,21 +8,19 @@ bot.get_updates(fail_silently: true) do |message|
   puts "@#{message.from.username}: #{message.text}"
   command = message.get_command_for(bot)
 
-  bot.listen do |message|
-    case message
-    when Telegram::Bot::Types::CallbackQuery
-      # Here you can handle your callbacks from inline buttons
-      if message.data == 'touch'
-        bot.api.send_message(chat_id: message.from.id, text: "Don't touch me!")
-      end
-    when Telegram::Bot::Types::Message
-      kb = [
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Go to Google', url: 'https://google.com'),
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Touch me', callback_data: 'touch'),
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Switch to inline', switch_inline_query: 'some text')
-      ]
-      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-      bot.api.send_message(chat_id: message.chat.id, text: 'Make a choice', reply_markup: markup)
+  message.reply do |reply|
+    case command
+    when /start/i
+      reply.text = "All I can do is say hello. Try the /greet command."
+    when /greet/i
+      reply.text = "Hello, #{message.from.first_name}. 🤖"
+    when /jumlah/i
+      reply.text = "Jumlah Material :, #{message.from.username}"
+    else
+      reply.text = "I have no idea what #{command.inspect} means."
     end
+    puts "sending #{reply.text.inspect} to @#{message.from.username}"
+    reply.send_with(bot)
+    
   end
 end
