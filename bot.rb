542dc42605496f5614086dd29aeb821987d9ade8
brianbarellaa <53068897+brianbarellaa@users.noreply.github.com>
2020-03-06 16:43:23 +0700

    Add files via upload

--- bot.rb
+++ bot.rb
@@ -1,5 +1,6 @@
 require 'telegram_bot'
-require "mysql2/em" 
+require 'mysql2/em'
+require 'csv' 
 
 token = '1136297398:AAEKKPU_f01jIa3ZIlvE7c1bka2a67uIPOA'
 
@@ -22,11 +23,13 @@ bot.get_updates(fail_silently: true) do |message|
     case command
     when /command1/i
       # Retrieve all rows from the persons table
+      reply.text = "Download laporan berikut"
       results = client.query("SELECT * FROM Monitor")
+
     when /command2/i
       reply.text = "Hello, #{message.from.first_name}. 🤖"
     when /command3/i
-      reply.text = "Jumlah Material :, #{message.from.username}"
+      reply.text = "Jumlah Material : #{message.from.username}"
     when /command4/i
       # We insert some data using MySQL multi-row insert
       client.query("insert into Monitor(Tiket) values('Mark'), ('Osman'), ('Conan')")
